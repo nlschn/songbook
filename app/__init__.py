4ec0fa4c591540a80c35ce4013ed55cec193b57c
Niklas Schneider <s8nlschn@stud.uni-saarland.de>
2022-08-20 15:38:16 +0200

    Try without logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
+# import logging
+# from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask, request, current_app
 from flask_sqlalchemy import SQLAlchemy
@@ -45,32 +45,32 @@ def create_app(config_class = Config):
     from app.playlists import bp as playlists_bp
     app.register_blueprint(playlists_bp, url_prefix='/playlists')
 
-    if not app.debug:
-        if app.config['MAIL_SERVER']:
-            auth = None
-            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-            secure = None
-            if app.config['MAIL_USE_TLS']:
-                secure = ()
-            mail_handler = SMTPHandler(
-                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-                credentials=auth, secure=secure)
-            mail_handler.setLevel(logging.ERROR)
-            app.logger.addHandler(mail_handler)
+    # if not app.debug:
+    #     if app.config['MAIL_SERVER']:
+    #         auth = None
+    #         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+    #             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    #         secure = None
+    #         if app.config['MAIL_USE_TLS']:
+    #             secure = ()
+    #         mail_handler = SMTPHandler(
+    #             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+    #             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+    #             toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+    #             credentials=auth, secure=secure)
+    #         mail_handler.setLevel(logging.ERROR)
+    #         app.logger.addHandler(mail_handler)
         
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/songbook.log', maxBytes = 10240, backupCount = 10)
-        file_handler.setFormatter(
-            logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+    #     if not os.path.exists('logs'):
+    #         os.mkdir('logs')
+    #     file_handler = RotatingFileHandler('logs/songbook.log', maxBytes = 10240, backupCount = 10)
+    #     file_handler.setFormatter(
+    #         logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    #     file_handler.setLevel(logging.INFO)
+    #     app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Songbook startup')
+    #     app.logger.setLevel(logging.INFO)
+    #     app.logger.info('Songbook startup')
     
     return app
 
